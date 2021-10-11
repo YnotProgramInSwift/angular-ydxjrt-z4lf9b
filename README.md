b22f5fa5639fe61e75ae53aa05809e2e88ad31fc
YnotProgramInSwift <byakugan84@hotmail.com>
2021-10-11 22:36:26 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ydxjrt-z4lf9b
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ydxjrt-z4lf9b)
---
+++ src/app/actor-list/actor-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { ActorListService } from './actor-list.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'my-actor-list-view',
+  templateUrl: './actor-list.component.html',
+  styles: [],
+})
+export class ActorListComponent {
+  actors;
+  showDetails;
+
+  constructor(
+    private actorListService: ActorListService,
+    private router: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.router.queryParamMap.subscribe((qpm) => {
+      if (qpm.has('showDetails')) {
+        this.showDetails = qpm.get('showDetails') === 'true' ? true : false;
+      } else {
+        this.showDetails = false;
+      }
+
+      this.actorListService.getActorList().subscribe((actors) => {
+        this.actors = actors;
+      });
+    });
+  }
+}
